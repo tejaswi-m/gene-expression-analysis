a50875d491c9489cf096930123cc25135b5c352d
tejaswi-m <tejaswiteju79@gmail.com>
2026-08-04 15:02:42 -0400

    GSE76826 analysis results

--- PBMC/GSE76826/main.py
+++ PBMC/GSE76826/main.py
@@ -20,38 +20,38 @@ from analyze_cell_population_proxies import run_cell_proxy_analysis
 from run_qc_normalize_r import run_qc_normalization
 
 mapping_text = """
-GSM2038455	S_HC_1
-GSM2038456	S_HC_2
-GSM2038457	S_HC_3
-GSM2038458	S_HC_4
-GSM2038459	S_HC_5
-GSM2038460	S_HC_6
-GSM2038461	S_HC_7
-GSM2038462	S_HC_8
-GSM2038463	S_HC_9
-GSM2038464	S_HC_10
-GSM2038465	S_HC_11
-GSM2038466	S_HC_12
-GSM2038467	S_LOD-dep_1
-GSM2038468	S_LOD-dep_2
-GSM2038469	S_LOD-dep_3
-GSM2038470	S_LOD-dep_4
-GSM2038471	S_LOD-dep_5
-GSM2038472	S_LOD-dep_6
-GSM2038473	S_LOD-dep_7
-GSM2038474	S_LOD-dep_8
-GSM2038475	S_LOD-dep_9
-GSM2038476	S_LOD-dep_10
-GSM2038477	S_LOD-rem_1
-GSM2038478	S_LOD-rem_2
-GSM2038479	S_LOD-rem_3
-GSM2038480	S_LOD-rem_4
-GSM2038481	S_LOD-rem_5
-GSM2038482	S_LOD-rem_6
-GSM2038483	S_LOD-rem_7
-GSM2038484	S_LOD-rem_8
-GSM2038485	S_LOD-rem_9
-GSM2038486	S_LOD-rem_10
+GSM2038455	HC_1
+GSM2038456	HC_2
+GSM2038457	HC_3
+GSM2038458	HC_4
+GSM2038459	HC_5
+GSM2038460	HC_6
+GSM2038461	HC_7
+GSM2038462	HC_8
+GSM2038463	HC_9
+GSM2038464	HC_10
+GSM2038465	HC_11
+GSM2038466	HC_12
+GSM2038467	LOD-dep_1
+GSM2038468	LOD-dep_2
+GSM2038469	LOD-dep_3
+GSM2038470	LOD-dep_4
+GSM2038471	LOD-dep_5
+GSM2038472	LOD-dep_6
+GSM2038473	LOD-dep_7
+GSM2038474	LOD-dep_8
+GSM2038475	LOD-dep_9
+GSM2038476	LOD-dep_10
+GSM2038477	LOD-rem_1
+GSM2038478	LOD-rem_2
+GSM2038479	LOD-rem_3
+GSM2038480	LOD-rem_4
+GSM2038481	LOD-rem_5
+GSM2038482	LOD-rem_6
+GSM2038483	LOD-rem_7
+GSM2038484	LOD-rem_8
+GSM2038485	LOD-rem_9
+GSM2038486	LOD-rem_10
 """
 
 
@@ -91,6 +91,8 @@ run_expression_analysis(
     case_label_keywords="case",
     control_label="Control",
     case_label="Case",
+    control_prefix="HC",
+    case_prefix="LOD",
 )
 
 run_cell_proxy_analysis(
@@ -104,6 +106,23 @@ run_cell_proxy_analysis(
     case_label_keywords="case",
     control_label="Control",
     case_label="Case",
+    control_prefix="HC",
+    case_prefix="LOD",
+)
+
+run_cell_proxy_analysis(
+    input_file=results_dir / "deduplicated_expression_dataset.csv",
+    metadata_file=TEST_DIR / "processed_data" / "sample_metadata.csv",
+    output_dir=TEST_DIR / "cell_population_proxies_neutrophil",
+    markers_csv=PROJECT_ROOT / "markers" / "blood_cell_markers.csv",
+    metadata_sample_column="analysis_sample",
+    metadata_phenotype_column="depression_status",
+    control_label_keywords="control",
+    case_label_keywords="case",
+    control_label="Control",
+    case_label="Case",
+    control_prefix="HC",
+    case_prefix="LOD",
 )
 
 print(f"Expression output: {expression_csv}")
